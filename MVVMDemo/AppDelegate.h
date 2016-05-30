47d73fe628024260181b3e831e17b8cb56aac82a
limitLiu <747638920@qq.com>
2016-05-30 08:55:37 +0800

    Initial Commit

---
+++ MVVMDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MVVMDemo
+//
+//  Created by 劉裕 on 30/5/2016.
+//  Copyright © 2016年 劉裕. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MVVMDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MVVMDemo
+//
+//  Created by 劉裕 on 30/5/2016.
+//  Copyright © 2016年 劉裕. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
